5508edf413be1c20b9cf0ca9f3775df8ff903bed
Gabriel Wong <contact@gabrielwong.net>
2013-10-17 18:16:38 -0400

    Fix glitch in bit mask where 1111 was used instead of 0x1111

--- src/net/gabrielwong/ultimate/game/Move.java
+++ src/net/gabrielwong/ultimate/game/Move.java
@@ -19,12 +19,12 @@ public class Move {
 	}
 	
 	public int getPlayerId(){
-		int mask = 1 << PLAYER_ID_BIT;
+		int mask = 0x1 << PLAYER_ID_BIT;
 		return mask & data;
 	}
 	
 	public void setPlayerId(int playerId){
-		int mask = 1 << PLAYER_ID_BIT;
+		int mask = 0x1 << PLAYER_ID_BIT;
 		if (playerId == 0){
 			data &= ~mask;
 		} else if (playerId == 1){
@@ -33,24 +33,22 @@ public class Move {
 	}
 	
 	public int getBigIndex(){
-		int mask = 1111 << BIG_INDEX_BIT;
-		return (mask & data) >> BIG_INDEX_BIT;
+		int mask = 0x1111 << BIG_INDEX_BIT;
+		return (mask & data) >>> BIG_INDEX_BIT;
 	}
 	
 	public void setBigIndex(int bigIndex){
-		int mask = 1111 << BIG_INDEX_BIT;
-		data &= ~mask; // Clear bits
+		data &= ~(0x1111 << BIG_INDEX_BIT); // Clear bits
 		data |= bigIndex << BIG_INDEX_BIT; // Set bits
 	}
 	
 	public int getSmallIndex(){
-		int mask = 1111 << SMALL_INDEX_BIT;
-		return (mask & data) >> SMALL_INDEX_BIT;
+		int mask = 0x1111 << SMALL_INDEX_BIT;
+		return (mask & data) >>> SMALL_INDEX_BIT;
 	}
 	
 	public void setSmallIndex(int smallIndex){
-		int mask = 1111 << SMALL_INDEX_BIT;
-		data &= ~mask;
+		data &= ~(0x1111 << SMALL_INDEX_BIT);
 		data |= smallIndex << SMALL_INDEX_BIT;
 	}
 }
