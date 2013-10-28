72099ffd364a7846bdb180cc74f03883668d0529
Gabriel Wong <contact@gabrielwong.net>
2013-10-28 02:12:49 -0400

    Changed framework of MainActivity to use fragments.

--- src/net/gabrielwong/ultimate/MainActivity.java
+++ src/net/gabrielwong/ultimate/MainActivity.java
@@ -1,32 +1,53 @@
 package net.gabrielwong.ultimate;
 
-import net.gabrielwong.ultimate.game.GameLogic;
-import net.gabrielwong.ultimate.graphics.BoardView;
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.Menu;
-import android.view.View;
 
-public class MainActivity extends Activity {
+import com.google.example.games.basegameutils.BaseGameActivity;
 
-	BoardView graphicView;
+public class MainActivity extends BaseGameActivity {
 	
+	// Debug stuff
+	final boolean ENABLE_DEBUG = true;
+	final String TAG = "MainActivity";
+	
+	// Fragments
+	GameplayFragment gameplayFragment = null;
+    
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		GameLogic logic = new GameLogic();
-		graphicView = new BoardView(this);
-		logic.addStateChangeListener(graphicView);
-		graphicView.addMoveListener(logic);
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            enableDebugLog(ENABLE_DEBUG, TAG);
+            setContentView(R.layout.activity_main);
+            
+            gameplayFragment = new GameplayFragment();
+            
+            switchToFragment(gameplayFragment);
+    }
+	
+	void switchToFragment(Fragment newFrag){
+		getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, newFrag)
+        	.commit();
+	}
+	
+
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+    }
+
+	@Override
+	public void onSignInFailed() {
+		// TODO Auto-generated method stub
 		
-		setContentView(graphicView);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onSignInSucceeded() {
+		// TODO Auto-generated method stub
+		
 	}
-
 }
